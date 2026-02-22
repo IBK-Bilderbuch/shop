c657c1c8913d4d101af2606fd24a7b0a53888506
IBK-Bilderbuch <aniantonyan@yahoo.com>
2026-02-22 22:09:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -282,12 +282,7 @@ def index():
     return render_template("index.html", kategorien=kategorien, user_email=session.get("user_email"))
 
 # Produkt Detail
-@app.route("/produkt/<int:produkt_id>")
-def produkt_detail(produkt_id):
-    produkt = next((p for p in produkte if p["id"] == produkt_id), None)
-    if not produkt:
-        abort(404)
-    return render_template("produkt.html", produkt=produkt, user_email=session.get("user_email"))
+
 
 
 @app.route('/produkt/<int:produkt_id>')
