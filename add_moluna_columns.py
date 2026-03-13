fdf3af3281633263ef34c21c2fcfbe32cce38f43
IBK-Bilderbuch <aniantonyan@yahoo.com>
2026-03-13 12:04:23 +0100

    Create add_moluna_columns.py

---
+++ add_moluna_columns.py
@@ -0,0 +1,13 @@
+from app import app, db
+from sqlalchemy import text
+
+with app.app_context():
+    db.session.execute(text("""
+        ALTER TABLE bestellungen
+        ADD COLUMN moluna_status VARCHAR(50),
+        ADD COLUMN moluna_order_id VARCHAR(100),
+        ADD COLUMN trackingnummer VARCHAR(100);
+    """))
+    db.session.commit()
+
+print("Moluna-Spalten wurden hinzugefügt!")
