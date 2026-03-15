8447434acd90cfce1844fbfceacbbb560ceaf7d4
IBK-Bilderbuch <aniantonyan@yahoo.com>
2026-03-15 09:41:21 +0100

    Update models.py

--- models.py
+++ models.py
@@ -26,6 +26,9 @@ class Bestellung(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), nullable=False)
 
+    collectkey = db.Column(db.String(100))   
+
+
     # ⭐ PERSONENDATEN
     vorname = db.Column(db.String(120))
     nachname = db.Column(db.String(120))
