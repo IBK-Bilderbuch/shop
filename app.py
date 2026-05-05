f1180b9da2a747ca13a8bc1aae82a02bbafd0a1d
IBK-Bilderbuch <aniantonyan@yahoo.com>
2026-05-05 13:13:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1236,6 +1236,10 @@ def newsletterbesteatigung():
 @app.route("/newsletteranmeldung")
 def newsletteranmeldung():
     return render_template("newsletteranmeldung.html", user_email=session.get("user_email"))
+
+@app.route("/widerruf")
+def widerruf():
+    return render_template("widerruf.html", user_email=session.get("user_email"))
     
 # ============================
 # INDEX HAUPTSEITE
