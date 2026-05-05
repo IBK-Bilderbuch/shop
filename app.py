a123c9a1bc24e57665cc9bdf88daeb04079d5809
IBK-Bilderbuch <aniantonyan@yahoo.com>
2026-05-05 15:40:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1049,7 +1049,7 @@ def submitwiderruf():
     anzahl = request.form.get("anzahl")
     warenbezeichnung = request.form.get("warenbezeichnung")
     datum = request.form.get("datum")
-    if not name or not email or not message or not anschrift or not anzahl or not warenbezeichnung or not datum:
+    if not name or not email or not anschrift or not anzahl or not warenbezeichnung or not datum:
         flash("Bitte fülle alle Felder aus!", "error")
         return redirect("/widerruf")
     try:
