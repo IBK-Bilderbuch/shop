2bd972a70b8c579de3cb7fc7f56261edc9187fe7
IBK-Bilderbuch <aniantonyan@yahoo.com>
2026-05-19 19:32:52 +0200

    Update models.py

--- models.py
+++ models.py
@@ -77,9 +77,7 @@ class NewsletterSubscriber(db.Model):
 # User Modell
 # ----------------------
 
-class Produkt(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
+
 
 
 class User(db.Model):
